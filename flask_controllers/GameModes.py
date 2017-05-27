600ec67b175ca78c4dd72b4468368920ce390316
dsandersAzure <dsanders_can@outlook.com>
2017-05-26 20:49:25 -0400

    Add text only mode to get game modes

--- flask_controllers/GameModes.py
+++ flask_controllers/GameModes.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask.views import MethodView
 from flask_helpers.build_response import build_response
 from flask_helpers.ErrorHandler import ErrorHandler
@@ -6,9 +7,18 @@ from python_cowbull_game.GameObject import GameObject
 
 class GameModes(MethodView):
     def get(self):
+        textonly = request.args.get('textmode', None)
+        if textonly:
+            return build_response(
+                html_status=200,
+                response_data=GameObject.game_modes,
+                response_mimetype="application/json"
+            )
+
         digits = GameObject.digits_used
         guesses = GameObject.guesses_allowed
-        game_modes = [mode for mode in GameObject.digits_used]
+        game_modes = GameObject.game_modes
+#        game_modes = [mode for mode in GameObject.digits_used]
 
         return_list = []
         for mode in game_modes:
