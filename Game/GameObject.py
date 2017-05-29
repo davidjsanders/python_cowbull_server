17087f0305140df5c594038c0277634c2ed55a2c
David Sanders <dsanderscanada@gmail.com>
2017-05-29 09:11:03 -0400

    Reset superclassed GameObject to use default modes

--- Game/GameObject.py
+++ Game/GameObject.py
@@ -2,19 +2,19 @@ from python_cowbull_game.GameObject import GameObject as BaseGameObject
 
 
 class GameObject(BaseGameObject):
-    game_modes = ["easy", "normal", "hard", "crazy", "mega"]
+#    game_modes = ["easy", "normal", "hard"]
 
-    digits_used = {
-        'easy': 3,
-        'normal': 4,
-        'hard': 6
-    }
+#    digits_used = {
+#        'easy': 3,
+#        'normal': 4,
+#        'hard': 6
+#    }
 
-    guesses_allowed = {
-        'easy': 15,
-        'normal': 10,
-        'hard': 6
-    }
+#    guesses_allowed = {
+#        'easy': 15,
+#        'normal': 10,
+#        'hard': 6
+#    }
 
     def __init__(self):
         super(GameObject, self).__init__()
