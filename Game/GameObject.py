d956d6dea303c7db6bf88a136fdf0515f8e422bd
David Sanders <dsanderscanada@gmail.com>
2017-05-31 13:24:59 -0400

    Added 'tough' mode to test endpoints

--- Game/GameObject.py
+++ Game/GameObject.py
@@ -2,19 +2,21 @@ from python_cowbull_game.GameObject import GameObject as BaseGameObject
 
 
 class GameObject(BaseGameObject):
-#    game_modes = ["easy", "normal", "hard"]
+    game_modes = ["easy", "normal", "hard", "tough"]
 
-#    digits_used = {
-#        'easy': 3,
-#        'normal': 4,
-#        'hard': 6
-#    }
+    digits_used = {
+        'easy': 3,
+        'normal': 4,
+        'hard': 6,
+        'tough': 10
+    }
 
-#    guesses_allowed = {
-#        'easy': 15,
-#        'normal': 10,
-#        'hard': 6
-#    }
+    guesses_allowed = {
+        'easy': 15,
+        'normal': 10,
+        'hard': 6,
+        'tough': 10
+    }
 
     def __init__(self):
         super(GameObject, self).__init__()
