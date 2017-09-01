d8193145ab44858d78a0c71db3993c4f9d93a7eb
David Sanders <dsanderscanada@gmail.com>
2017-08-31 20:33:17 -0400

    Refactor GameModes to take advantage of GameObject properties.

--- flask_controllers/GameModes.py
+++ flask_controllers/GameModes.py
@@ -1,7 +1,6 @@
 from flask import request
 from flask.views import MethodView
 from flask_helpers.build_response import build_response
-from flask_helpers.ErrorHandler import ErrorHandler
 from python_cowbull_game.GameObject import GameObject as GameObject
 
 
@@ -11,45 +10,15 @@ class GameModes(MethodView):
 
         textonly = request.args.get('textmode', None)
         if textonly:
-            return build_response(
-                html_status=200,
-                response_data=game_object.game_modes,
-                response_mimetype="application/json"
-            )
-
-        game_modes = [{"mode": game_object.game_types[gt].mode,
-                       "digits": game_object.game_types[gt].digits,
-                       "guesses": game_object.game_types[gt].guesses
-                       } for gt in game_object.game_types]
+            response_data = game_object.game_modes
+        else:
+            response_data = [{"mode": game_object.game_types[gt].mode,
+                              "digits": game_object.game_types[gt].digits,
+                              "guesses": game_object.game_types[gt].guesses
+                             } for gt in game_object.game_types]
 
         return build_response(
             html_status=200,
-            response_data=game_modes,
-            response_mimetype="application/json"
-        )
-
-        game_modes = [{"mode": game_object.game_types[gt].mode,
-                       "digits": game_object.game_types[gt].digits,
-                       "guesses": game_object.game_types[gt].guesses_allowed
-                       } for gt in game_object.game_types]
-
-        digits = game_object.digits_used
-        guesses = game_object.guesses_allowed
-        game_modes = game_object.game_modes
-#        game_modes = [mode for mode in GameObject.digits_used]
-
-        return_list = []
-        for mode in game_modes:
-            return_list.append(
-                {
-                    "mode": mode,
-                    "digits": digits[mode],
-                    "guesses": guesses[mode]
-                }
-            )
-
-        return build_response(
-            html_status=200,
-            response_data=return_list,
+            response_data=response_data,
             response_mimetype="application/json"
         )
