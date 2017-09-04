a8244c25c5cc7e2279723538daa9889a1d327cae
David Sanders <dsanderscanada@gmail.com>
2017-09-04 17:03:21 -0400

    Remove new modes for testing.

--- extensions/ExtGameController.py
+++ extensions/ExtGameController.py
@@ -4,10 +4,10 @@ from python_cowbull_game.GameMode import GameMode
 
 class ExtGameController(GameController):
     additional_modes = [
-        GameMode(mode="SuperTough", priority=6, digits=10, digit_type=0),
-        GameMode(mode="hexTough", priority=5, digits=3, guesses_allowed=3, digit_type=1)
+#        GameMode(mode="SuperTough", priority=6, digits=10, digit_type=0),
+#        GameMode(mode="hexTough", priority=5, digits=3, guesses_allowed=3, digit_type=1)
     ]
 
-    def __init__(self, game=None):
-        super(ExtGameController, self).__init__(game=game)
+    def __init__(self):
+        super(ExtGameController, self).__init__()
         self.add_mode(self.additional_modes)
