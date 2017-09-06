7cf4578297667bc8b6405e99828818de87e3ba9a
David Sanders <dsanderscanada@gmail.com>
2017-09-06 14:41:50 -0400

    Update extensions and GameController subclass

--- extensions/ExtGameController.py
+++ extensions/ExtGameController.py
@@ -8,5 +8,9 @@ class ExtGameController(GameController):
         GameMode(mode="hexTough", priority=5, digits=3, guesses_allowed=3, digit_type=1)
     ]
 
-    def __init__(self):
-        super(ExtGameController, self).__init__(game_modes=self.additional_modes)
+    def __init__(self, mode=None, game_json=None):
+        super(ExtGameController, self).__init__(
+            game_json=game_json,
+            mode=mode,
+            game_modes=self.additional_modes
+        )
