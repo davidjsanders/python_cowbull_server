0d1458e82f04e5642f487d571cb2623e8f18f792
David Sanders <dsanderscanada@gmail.com>
2017-09-06 15:16:42 -0400

    Fix POST method of GameServerController

--- flask_controllers/GameServerController.py
+++ flask_controllers/GameServerController.py
@@ -185,7 +185,7 @@ class GameServerController(MethodView):
             self.handler.log(message="Loaded game: {} ({})".format(_loaded_game, type(_loaded_game)))
 
             self.handler.log(message="About to load mode.")
-            _mode = dict(_loaded_game)["mode"]
+            _mode = dict(**_loaded_game)
             self.handler.log(message="Loaded game mode: {}".format(_mode))
 
             _game = GameController(
