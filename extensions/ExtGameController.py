76b0c364b8bfbc553d3eedc97e4805299b8d9974
David Sanders <dsanderscanada@gmail.com>
2017-09-06 16:06:19 -0400

    Update to include instruction and help texts in GET response.

--- extensions/ExtGameController.py
+++ extensions/ExtGameController.py
@@ -23,7 +23,21 @@ class ExtGameController(GameController):
     # ]
     #
 
-    additional_modes = []
+    additional_modes = [
+        GameMode(
+            mode="hexTough",
+            priority=5,
+            digits=3,
+            guesses_allowed=3,
+            digit_type=1,
+            help_text="Guess a set of 3 digits between 0 and F",
+            instruction_text="hexTough is a hard hexidecimal based game. You need to "
+                             "guess 3 digits, each of which needs to be a hex number "
+                             "(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, or F). "
+                             "The numbers can be passed as hex (0x0, 0xd, 0xE) or as "
+                             "strings (A, b, C, 0, 5, etc.)."
+        )
+    ]
 
     def __init__(self, game_modes=None, mode=None, game_json=None):
         if game_modes is not None and not isinstance(game_modes, list):
