683c66654aac71ee1804857a226fa0cb6d1262ce
David Sanders <dsanderscanada@gmail.com>
2017-09-07 09:45:02 -0400

    Update GameModes GET to add debugging

--- flask_controllers/GameModes.py
+++ flask_controllers/GameModes.py
@@ -1,3 +1,5 @@
+import logging
+
 from flask import request
 from flask.views import MethodView
 from flask_helpers.build_response import build_response
@@ -11,17 +13,23 @@ from extensions.ExtGameController import ExtGameController as GameController
 
 class GameModes(MethodView):
     def get(self):
+        logging.debug("GameModes: GET: Initializing GameObject")
         game_object = GameController(mode=None)
+        logging.debug("GameModes: GET: GameObject initialized to {}".format(game_object.save()))
 
+        logging.debug("GameModes: GET: Checking if textmode flag set")
         if request.args.get('textmode', None):
+            logging.debug("GameModes: GET: Responding with list of names")
             response_data = game_object.game_mode_names
         else:
+            logging.debug("GameModes: GET: Responding with JSON object")
             response_data = [{"mode": gt.mode,
                               "digits": gt.digits,
                               "digit-type": gt.digit_type,
                               "guesses": gt.guesses_allowed
                              } for gt in game_object.game_modes]
 
+        logging.debug("GameModes: GET: Return {}".format(response_data))
         return build_response(
             html_status=200,
             response_data=response_data,
