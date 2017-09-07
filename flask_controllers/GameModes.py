7dfc059dc7c89aec7a006444f5b28985f97df229
David Sanders <dsanderscanada@gmail.com>
2017-09-07 15:02:26 -0400

    Update GameModes to provide instructions and notes when modes requested.

--- flask_controllers/GameModes.py
+++ flask_controllers/GameModes.py
@@ -20,11 +20,29 @@ class GameModes(MethodView):
             response_data = game_object.game_mode_names
         else:
             logging.debug("GameModes: GET: Responding with JSON object: {}".format(game_object.game_modes))
-            response_data = [{"mode": gt.mode,
-                              "digits": gt.digits,
-                              "digit-type": gt.digit_type,
-                              "guesses": gt.guesses_allowed
-                             } for gt in game_object.game_modes]
+            response_data = {
+                "instructions": "Welcome to the CowBull game. The objective of this game "
+                                "is to guess a set of digits by entering a sequence of "
+                                "numbers. Each time you guess (there is a set number of "
+                                "tries to win), you will be provided with an analysis "
+                                "of your guesses.",
+                "notes": "The modes can be different depending upon the game server that "
+                         "serves the game.",
+                "modes": [
+                    {
+                        "mode": gt.mode,
+                        "digits": gt.digits,
+                        "digit-type": gt.digit_type,
+                        "guesses": gt.guesses_allowed
+                    }
+                    for gt in game_object.game_modes
+                ]
+            }
+#            response_data = [{"mode": gt.mode,
+#                              "digits": gt.digits,
+#                              "digit-type": gt.digit_type,
+#                              "guesses": gt.guesses_allowed
+#                             } for gt in game_object.game_modes]
 
         logging.debug("GameModes: GET: Return {}".format(response_data))
         return build_response(
