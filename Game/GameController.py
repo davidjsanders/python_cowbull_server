3c4e7e69cfa5e3d476aa60f9c25496486ad83d52
David Sanders <dsanderscanada@gmail.com>
2017-09-07 10:16:42 -0400

    Update GameController to remove any 0x from hex digits being displayed to caller.

--- Game/GameController.py
+++ Game/GameController.py
@@ -266,11 +266,15 @@ class GameController(object):
         """Simple method to form a start again message and give the answer in readable form."""
         logging.debug("Start again message delivered: {}".format(message))
         the_answer = ', '.join(
-            [str(d).replace('0x', '') for d in self.game.answer][:-1]
-        ) + ', and ' + [str(d) for d in self.game.answer][-1]
+            [self._non_hex(d) for d in self.game.answer][:-1]
+        ) + ', and ' + [self._non_hex(d) for d in self.game.answer][-1]
 
         return "{0}{1} The correct answer was {2}. Please start a new game.".format(
             message,
             "." if message[-1] not in [".", ",", ";", ":", "!"] else "",
             the_answer
         )
+
+    @staticmethod
+    def _non_hex(value):
+        return str(value).replace('0x', '')
