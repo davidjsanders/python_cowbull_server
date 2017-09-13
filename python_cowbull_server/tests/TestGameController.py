390bfd5c9412b08bf06e2f792dedb19950282b3f
David Sanders <dsanderscanada@gmail.com>
2017-09-13 12:49:12 -0400

    Add unittest framework

---
+++ python_cowbull_server/tests/TestGameController.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from Game.GameController import GameController
+
+
+class TestGameController(TestCase):
+    def setUp(self):
+        pass
+
+    def test_instantiate(self):
+        g = GameController()
+
+    def tearDown(self):
+        pass
---
+++ python_cowbull_server/tests/__init__.py
@@ -0,0 +1,2 @@
+from python_cowbull_server.tests.TestGameController import TestGameController
+from python_cowbull_server.tests.TestGameObject import TestGameObject
