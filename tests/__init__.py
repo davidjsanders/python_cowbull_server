4e76d480299927e921951668a6f6afe0be1bf557
dsandersAzure <dsanders_can@outlook.com>
2017-09-19 10:15:35 -0400

    Place tests in parent package

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+# Import required tests
+from tests.TestV1Routes import TestV1Routes
+from tests.TestGameController import TestGameController
+from tests.TestGameObject import TestGameObject
+from tests.TestGameMode import TestGameMode
+from tests.TestErrorHandler import TestErrorHandler
