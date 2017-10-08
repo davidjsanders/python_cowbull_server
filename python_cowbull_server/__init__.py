55af5785d1aedff028f85229af691d5f59ba434a
David Sanders <dsanderscanada@gmail.com>
2017-10-08 16:28:11 -0400

    Remove parameter support (due to unittest module) and modify to print parameters on each run.

--- python_cowbull_server/__init__.py
+++ python_cowbull_server/__init__.py
@@ -2,41 +2,28 @@
 # app, this code allows the app created to be imported into any other
 # package, module, or method.
 
-import argparse
 import logging          # Import standard logging - for levels only
 
 from python_cowbull_server.Configurator import Configurator
 from flask import Flask
 
-#
-# Step 1 - Check any command line arguments passed
-#
-parser = argparse.ArgumentParser()
-parser.add_argument('--env',
-                    dest='showenvvars',
-                    default=False,
-                    action='store_true',
-                    help="Show the environment variables that can be set by this app")
-
-args = parser.parse_args()
-
 # Instantiate the Flask application as app
 app = Flask(__name__)
 c = Configurator()
 
-# Decide if using ANSI or GUI
-if args.showenvvars:
-    print('')
-    print('The following environment variables may be set to dynamically')
-    print('configure the server. Alternately, these can be defined in a ')
-    print('file and passed using the env. var. COWBULL_CONFIG.')
-    print('')
-    print('Please note. Env. Vars can be *ALL* lowercase or *ALL* uppercase.')
-    print('')
-    for name, desc in c.get_variables():
-        print(name, '-->', desc)
-    print('')
-    exit(0)
+print('')
+print('-'*80)
+print('The following environment variables may be set to dynamically')
+print('configure the server. Alternately, these can be defined in a ')
+print('file and passed using the env. var. COWBULL_CONFIG.')
+print('')
+print('Please note. Env. Vars can be *ALL* lowercase or *ALL* uppercase.')
+print('-'*80)
+print('')
+for name, desc in c.get_variables():
+    print(name, '-->', desc)
+print('-'*80)
+print('')
 
 c.execute_load(app)
 
