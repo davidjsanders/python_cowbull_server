add61a64128971dc0976597742d4771430c68422
dsandersAzure <dsanders_can@outlook.com>
2017-11-21 16:58:14 -0500

    Modifying GCP Storage Persist for loading

--- Persistence/GCPStoragePersist.py
+++ Persistence/GCPStoragePersist.py
@@ -1,5 +1,6 @@
 from flask_helpers.ErrorHandler import ErrorHandler
 from io import StringIO
+from io import BytesIO
 from six import text_type
 
 import googleapiclient.discovery
@@ -52,7 +53,11 @@ class GCPStoragePersist:
             media_body=googleapiclient.http.MediaIoBaseUpload(contents, 'application/json')
         )
         self.handler.log(message="Executing GCP Storage request", status=0)
-        req.execute()
+        try:
+            resp = req.execute()
+            print(resp)
+        except Exception as e:
+            print(e)
         self.handler.log(message="Closed temp file/stream with game data", status=0)
 
     def load(self, key=None):
@@ -62,16 +67,42 @@ class GCPStoragePersist:
         if key is None:
             raise ValueError("Key must be present to execute_load game")
 
+        contents = BytesIO()
         self.handler.log(message="Requesting object {}".format(key))
-        return_result = self.storage_client.objects().get_media(
-            bucket=self.bucket,
-            object=key
-        )
+
+        try:
+            self.handler.log(message="In try for ".format(key))
+            return_result = self.storage_client.objects().get_media(
+                bucket=self.bucket,
+                object=key
+            )
+            self.handler.log(message="After fetch for ".format(key))
+        except Exception as e:
+            print(repr(e))
+
+        self.handler.log(message="Requesting downloader for {}".format(key))
+        try:
+            downloader = googleapiclient.http.MediaIoBaseDownload(contents, return_result)
+        except Exception as e:
+            print(repr(e))
+
+        self.handler.log(message="Downloader --> {}".format(downloader))
+
+        done = False
+        while done is False:
+            self.handler.log(message="Fetching output")
+            try:
+                status, done = downloader.next_chunk()
+            except Exception as e:
+                print(repr(e))
+            self.handler.log(message="Fetching. Status is {}".format(status))
 
         self.handler.log(message="Checking datatype of return object")
         if return_result is not None:
             if isinstance(return_result, bytes):
                 return_result = str(return_result.decode('utf-8'))
 
-        self.handler.log(message="Returning result")
+        return_result = contents.read()
+        contents.close()
+        self.handler.log(message="Returning result: {}".format(return_result))
         return return_result
