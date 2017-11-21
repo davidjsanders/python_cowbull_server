5eb6a1d50fed5f7e012f96dc197be93a36132735
David Sanders <dsanderscanada@gmail.com>
2017-11-21 14:10:35 -0500

    Update configuration to correctly default PERSISTER

--- python_cowbull_server/Configurator.py
+++ python_cowbull_server/Configurator.py
@@ -23,7 +23,7 @@ class Configurator(object):
                 "name": "PERSISTER",
                 "description": "The persistence engine object",
                 "required": False,
-                "default": '{"engine": "redis", "parameters": {"host": "localhost", "port": 6379, "db": 0}}',
+                "default": '{"engine_name": "redis", "parameters": {"host": "localhost", "port": 6379, "db": 0}}',
                 "caster": PersistenceEngine
             },
             {
