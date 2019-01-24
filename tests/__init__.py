fbd19c44ff478cffba3939da3a9aca01c472512d
David Sanders <dsanderscanada@gmail.com>
2019-01-23 20:30:25 -0500

    Remove cached files

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# Import required tests - Note new package placement.
-from tests.TestV1Routes import TestV1Routes
-from tests.TestGameController import TestGameController
-from tests.TestGameObject import TestGameObject
-from tests.TestGameMode import TestGameMode
-from tests.TestErrorHandler import TestErrorHandler
