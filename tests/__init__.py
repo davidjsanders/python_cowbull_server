79abd17dfb790679087125c74e32f648d3947833
David Sanders <dsanderscanada@gmail.com>
2019-01-27 20:25:23 -0500

    Update unit tests module

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# Import required tests - Note new package placement.
-from tests.TestV1Routes import TestV1Routes
-from tests.TestGameController import TestGameController
-from tests.TestGameObject import TestGameObject
-from tests.TestGameMode import TestGameMode
-from tests.TestErrorHandler import TestErrorHandler
-from tests.TestConfigurator import TestConfigurator
-from tests.TestHelpers import TestHelpers
