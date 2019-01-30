f05554c5451f76e2eb62b606a0a2f67ae1430ff6
David Sanders <dsanderscanada@gmail.com>
2019-01-29 20:45:14 -0500

    Remove individual test scripts

--- systest.py
+++
@@ -1,5 +0,0 @@
-import unittest
-from systests import *
-
-if __name__ == '__main__':
-    unittest.main()
--- test_app.py
+++
@@ -1,5 +0,0 @@
-import unittest
-from unittests import *
-
-if __name__ == '__main__':
-    unittest.main()
