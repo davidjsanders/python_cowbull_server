48a1bcae2ba17e30664234495aaa58a35baf7b80
David Sanders <dsanderscanada@gmail.com>
2020-04-02 14:07:23 -0400

    Add Redis for systests

--- cloudbuild_test.sh
+++ cloudbuild_test.sh
@@ -5,5 +5,7 @@ coverage run unittests/main.py
 coverage xml -i
 export PERSISTER='{"engine_name": "redis", "parameters": {"host": "localhost", "port": 6379, "db": 0, "password": ""}}'
 export LOGGING_LEVEL=30
+docker run -d --rm --name redis -p 6379:6379 redis:alpine3.11
 python systests/main.py
+docker stop redis
 exit $?
