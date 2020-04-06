ac8d713a405435dbd531208fea998d5ec42f5753
David Sanders <dsanderscanada@gmail.com>
2020-04-06 12:40:56 -0400

    Update GAE configuration

--- PersistenceExtensions/GAEStorage.py
+++ PersistenceExtensions/GAEStorage.py
@@ -1,20 +1,38 @@
 from flask_helpers.ErrorHandler import ErrorHandler
+from google.auth.credentials import Credentials
 from google.cloud import storage
 from google.cloud.storage import Blob
-from io import StringIO
-from io import BytesIO
+from google.oauth2 import service_account
 from Persistence.AbstractPersister import AbstractPersister
-from six import text_type
+
+
+import google.auth
 
 # import cloudstorage
 
 
 class Persister(AbstractPersister):
-    def __init__(self, bucket=None, credentials_file=None):
+    def __init__(self, bucket=None, credentials_file=None, project=None):
+        if bucket is None:
+            raise ValueError("Bucket name must be provided to the persister.")
+        if credentials_file is not None and project is None:
+            raise ValueError("Project must be provided if credentials file is provided!")
+
         super(Persister, self).__init__()
 
         self.handler.module="GAEStorage"
         self.handler.log(message="Validating if credentials are defined or if defaults should be used")
+        if credentials_file is not None:
+            self.handler.log(message="Setting credentials")
+            self.credentials = service_account.Credentials.from_service_account_file(credentials_file)
+            self.storage_client = storage.Client(
+                credentials=self.credentials,
+                project=project
+            )
+        else:
+            self.handler.log(message="Using default credentials")
+            self.credentials, project = google.auth.default()
+            self.storage_client = storage.Client()
 
         self.storage_client = storage.Client()
         self.bucket = self.storage_client.bucket("dasander-cowbull-save-games")
@@ -27,4 +45,7 @@ class Persister(AbstractPersister):
     def save(self, key=None, jsonstr=None):
         super(Persister, self).save(key=key, jsonstr=jsonstr)
         blob = self.bucket.blob(key)
-        blob.upload_from_string(jsonstr)
+        blob.upload_from_string(
+            data=jsonstr,
+            content_type="application/json"
+        )
