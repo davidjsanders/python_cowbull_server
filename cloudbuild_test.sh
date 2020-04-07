ddde04ee2a91a5e9f9420151e6a8c3e39fdecabd
David Sanders <dsanderscanada@gmail.com>
2020-04-07 19:30:14 -0400

    Remove cloudbuild test stage

--- cloudbuild_test.sh
+++ cloudbuild_test.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
-pip install -r requirements.txt
-export PYTHONPATH="$(pwd)"
-coverage run unittests/main.py
-coverage xml -i
+echo "Stage 1 Testing temporarily suspended"
+# pip install -r requirements.txt
+# export PYTHONPATH="$(pwd)"
+# coverage run unittests/main.py
+# coverage xml -i
 # export PERSISTER='{"engine_name": "redis", "parameters": {"host": "localhost", "port": 6379, "db": 0, "password": ""}}'
 # export LOGGING_LEVEL=30
 # docker run -d --rm --name redis -p 6379:6379 redis:alpine3.11
