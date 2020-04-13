6028c617e27238ba0accff899f2916c0692e7b24
David Sanders <dsanderscanada@gmail.com>
2020-04-13 16:19:26 -0400

    Enable TLS and Cert Manager

---
+++ vendor/k8s/cluster-prep.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+echo
+echo "NGINX Ingress Controller installation."
+echo "For reference: https://kubernetes.github.io/ingress-nginx/deploy/"
+echo
+echo "Apply cloud generic"
+kubectl apply \
+    -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/cloud-generic.yaml
+echo "Create cluster role binding for GKE"
+kubectl create \
+    clusterrolebinding cluster-admin-binding \
+    --clusterrole cluster-admin\
+    --user $(gcloud config get-value account)
+echo "Apply mandatory"
+kubectl apply \
+    -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
+echo "Apply generic"
+kubectl apply \
+    -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/provider/cloud-generic.yaml
+
+echo
+echo "Cert manager installation."
+echo "For reference: https://cert-manager.io/docs/installation/kubernetes/"
+echo "Note         : Version is set to 0.13.1"
+echo
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.13.1/cert-manager.yaml
